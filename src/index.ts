f954eb8db7b73e35ffb46f837488fcb1f4860cbe
byTimo <race.timo@gmail.com>
2020-03-27 23:18:24 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {Game, Types} from "phaser";
+
+const config: Types.Core.GameConfig = {
+    type: Phaser.AUTO, // Which renderer to use
+    width: 800, // Canvas width in pixels
+    height: 600, // Canvas height in pixels
+    parent: "game-container", // ID of the DOM element to add the canvas to
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+
+    }
+}
+
+const game = new Game(config);
+
+function preload() {
+    // Runs once, loads up assets like images and audio
+}
+
+function create() {
+    // Runs once, after all assets in preload are loaded
+}
+
+function update(time: any, delta: any) {
+    // Runs once per frame for the duration of the scene
+}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    mode: "development",
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [new HtmlWebpackPlugin()]
+};
